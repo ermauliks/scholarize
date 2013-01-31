aca20c37fc72a1fc9057d4c22f1e0bc2b0855919
mauliks <mauliks@thoughtworks.com>
2013-01-31 15:06:35 +0530

    initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarize::Application.config.secret_token = '89dc82f700e975f940776c5798c377a59e30364751044ed88d3f6e3d2e6a801fca892aa9f3552bcf50297423eb3dd11e5ff5f22a9532a4a773a9e1473b2ea90d'
