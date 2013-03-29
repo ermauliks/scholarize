a219d150b43a36b1381e3877c987f12da63487a0
Abhishek <asharma@thoughtworks.com>
2013-03-30 02:27:58 +0530

    Abhi #3 Added Seed Data for Scholarships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Scholarship.delete_all
+Scholarship.create(:title => 'ThoughtWorks Scholarship',
+               :description =>
+                   %{<p>
+        <em>ThoughtWorks Scholarship</em> aims at rewarding the under-privileged and promising students by
+        paying for their education. Last date to apply for the scholarship is 21/04/2013.
+        </p>},
+               :amount =>  5000.00,
+               :quantity => 100)
+
+Scholarship.create(:title => 'Scholarship For Primary School Education',
+               :description =>
+                   %{<p>
+        Purpose of this scholarship is to <em>take care of educational expenses for the students residing in rural areas.</em>
+        Last date to apply for the scholarship is 22/04/2013.
+        </p>},
+               :amount =>  10000.00,
+               :quantity => 300)
+
+Scholarship.create(:title => 'Scholarship for Girls',
+               :description =>
+                   %{<p>
+       Intention of this scholarship is to <em>pay for the educational expenses for the girl students residing in rural areas.</em>
+      Last date to apply for the scholarship is 22/04/2013.
+      </p>},
+               :amount =>  50000.00,
+               :quantity => 500)
